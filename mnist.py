9c5fba14e2c72873a4318c6d02507dbb09cddc6d
aopanda <aoi.tanaka.ivanovic@gmail.com>
2023-05-23 09:50:17 +0900

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -19,8 +19,8 @@ app = Flask(__name__)
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-model = load_model('./model.h5')#学習済みモデルをロード
-
+#model = load_model('./model.h5') 
+model = tf.keras.models.load_model('./model.h5', compile=False)
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
